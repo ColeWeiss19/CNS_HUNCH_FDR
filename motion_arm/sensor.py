2e249e70bb2d132bf9e9d85526e1eff4fc24a99f
ColeWeiss19 <78231520+ColeWeiss19@users.noreply.github.com>
2024-03-20 19:19:59 -0400

    Update sensor.py

--- motion_arm/sensor.py
+++ motion_arm/sensor.py
@@ -1,25 +1,39 @@
 from machine import Pin, ADC, PWM
-from time import sleep
+from time import sleep, sleep_ms
+from statistics import stdev, mean
 
-button = Pin(1, Pin.IN, Pin.PULL_UP)
 touch = ADC(Pin(28))
-touch_value = touch.read_u16()
-touch_list = []
+data = []
+sigVal = 0
 
+# gathers baseline data into one list
 def calibrate(data):
     print("press button to begin calibration")
-    while data == []:    
-        if button.value() == 0:
-            print("do not press sensor")
-            sleep(1)
-            for i in range(50):
-                for i in range(20):
-                    touch_value = touch.read_u16()    
-                data.append(touch_value)
-                sleep(0.05)                    
-            print("calibration complete")
+    
+    data.clear()
+        
+    print("do not press sensor")
+        
+    # collecting baseline data
+    for i in range(50):
+        touch_value = touch.read_u16()
+        data.append(touch_value)
+        sleep(0.1)
+        
+    print("calibration complete")
+        
+    sigVal = stdev(data()) * 7.0 + mean(data())
 
+# acting as an accessor method
+def data():
+    return data
+
+# gathers sensor data and returns when the value is signifigant
 def search(sensor):
+    
     touch_value = sensor.read_u16()
+    
     sleep(0.5)
-    return touch_value
+    
+    if touch_value > sigVal:
+        return True
