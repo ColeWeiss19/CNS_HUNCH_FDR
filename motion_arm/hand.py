004f6c46b674fb42fb7fd10f40abe0cf1348e933
ColeWeiss19 <78231520+ColeWeiss19@users.noreply.github.com>
2024-04-04 14:27:38 -0400

    Update hand.py

--- motion_arm/hand.py
+++ motion_arm/hand.py
@@ -1,36 +1,25 @@
 #360-degree servo code
 
 from sensor import *
+import time
 
-servo = PWM(Pin(0)) # setting the Pin to PWM
+#servo = PWM(Pin(0)) # setting the Pin to PWM
 
 #------------------#
 #time.sleep_ms(2000) # optional for delay before starting
 #------------------#
 
 def closeHand(servo):
-
     servo.duty_u16(9000) # setting the servo to max speed in + direction
-
     time.sleep_ms(1550) # delay for 1.55 seconds
-
     servo.duty_u16(0) # stopping servo
 
 def openHand(servo):
-    
     servo.duty_u16(50) # setting the servo to max speed in - direction
-
     time.sleep_ms(1550) # delay for 1.55 seconds
-
     servo.duty_u16(0) # stopping servo
     
-def readAndClose(servo):
-    
-    if search() == True:
-        closeHand(servo)
-    
-def reset():
-    
+def reset(servo):
     openHand(servo)
-    
-    calibrate(data())
+    sigVal = calibrate()
+    return sigVal
