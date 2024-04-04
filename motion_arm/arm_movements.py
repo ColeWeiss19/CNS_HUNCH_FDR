d053e1390808a58d4321dcaf5a23a2dff45a476d
ColeWeiss19 <78231520+ColeWeiss19@users.noreply.github.com>
2024-04-04 14:27:14 -0400

    Update arm_movements.py with test code

--- motion_arm/arm_movements.py
+++ motion_arm/arm_movements.py
@@ -3,14 +3,20 @@
 from machine import Pin, PWM
 import math
 import random
-
-servo = PWM(Pin(1))
-servo.freq(50)
+import time
 
 def randomMotion(servo):
+    # defining servo frequency for PWM
+    servo.freq(50)
+    
+    # creating random angles and outputting to servo
     randomRadian = (((math.pi/2) * random.randint(-100, 100)))/100
-    position = (randomRadian * (180/math.pi)) * 50
+    degrees = math.ceil(randomRadian * (180/math.pi))
+    position = abs(degrees * 50)
     servo.duty_u16(position)
+    
+    # giving the servo time to turn to the angle
+    time.sleep(0.5)
 
 # To set the servo to a neutral position:
 #--- servo.duty_u16(4500) ---#
